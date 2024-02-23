73d2e82678f7cd3925e7b9ebfaf86d782eddeacb
thanhd <thanhdanh@icd-vn.com>
2024-02-23 17:02:41 +0700

    update: update login form

---
+++ src/components/forms/login.tsx
@@ -0,0 +1,72 @@
+import { type TLogin } from "@/common/validation/auth";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "../ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+export default function LoginForm() {
+  const loginForm = useForm<TLogin>({});
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email or username along with your password to log in.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...loginForm}>
+          <form action="">
+            <FormField
+              control={loginForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Login</Button>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,57 @@
+import { loginSchema, signUpSchema } from "@/common/validation/auth";
+import bcrypt from "bcrypt";
+
+import { TRPCError } from "@trpc/server";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const userRouter = createTRPCRouter({
+  signUp: publicProcedure
+    .input(signUpSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { email, password, username } = input;
+
+      const exists = await ctx.db.user.findFirst({
+        where: { email },
+      });
+
+      if (exists) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "User already exists.",
+        });
+      }
+
+      try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await ctx.db.user.create({
+          data: { email, password: hashedPassword, username },
+        });
+
+        return {
+          status: 201,
+          message: "Account created successfully",
+          result: result.email,
+        };
+      } catch (error) {}
+    }),
+
+  login: publicProcedure.input(loginSchema).mutation(async ({ ctx, input }) => {
+    const form = await loginSchema.parseAsync(input);
+    const user = await ctx.db.user.findFirst({
+      where: { email: form.email },
+    });
+    if (!user) {
+      return null;
+    }
+
+    const isValidPassword = await bcrypt.compare(form.password, user.password!);
+
+    if (!isValidPassword) {
+      return null;
+    }
+
+    return user;
+  }),
+});
